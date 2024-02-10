efcecf08b4ab3e2ec5a17840b1a37b9a8508e52e
berdibek-2002 <uralov0212@gmail.com>
2024-02-10 19:30:01 +0500

    version 1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import AppLayout from './component/layout/AppLayout'
 
-const contentStyle = {
-	textAlign: 'center',
-	minHeight: 'calc(100vh-60px)',
-	color: '#fff',
-	backgroundColor: '#001529',
-}
 export default function App() {
 	return <AppLayout />
 }
